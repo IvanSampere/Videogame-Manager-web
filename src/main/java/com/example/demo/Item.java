c27a6532b683073ccb32526891f50e6b00e58feb
Ivan Sampere <ivansampere@hotmail.com>
2020-04-18 13:57:41 +0200

    Revert "Se han implementado las funciones de añadir y eliminar habilidades del objeto "Item", y tambien se ha hecho el mapeo de objetos "OnetoMany" del item->hability. Se han hecho las pruebas y los imports y todo funciona bien."
    This reverts commit 7275172b71fc5c44d854caa57f22af6ebc2743a0.

--- src/main/java/com/example/demo/Item.java
+++ src/main/java/com/example/demo/Item.java
@@ -1,20 +1,13 @@
 package com.example.demo;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -24,16 +17,13 @@ public abstract class Item {
 	@Id
 	protected String name;
 	protected int value;
-	
-	@OneToMany
-	@JoinColumn(name = "item")
-	protected List<Hability> accions = new ArrayList<Hability>();
+//	protected List<Hability> accions ;
 	
 	Item(){
+		
 	}
 	
-
-//	GETTERS AND SETTERS
+	
 	public String getName() {
 		return name;
 	}
@@ -50,21 +40,21 @@ public abstract class Item {
 		this.value = value;
 	}
 
-	public List<Hability> getAccions() {
-		return accions;
-	}
+//	public List<Hability> getAccions() {
+//		return accions;
+//	}
+//
+//
+//	public void setAccions(List<Hability> accions) {
+//		this.accions = accions;
+//	}
 
 
-	public void setAccions(List<Hability> accions) {
-		this.accions = accions;
-	}
-
-	
-//	METHODS
 	@Override
 	public String toString() {
-		return "Item [name=" + name + ", value=" + value + ", accions=" + accions + "]";
+		return "Item [name=" + name + ", value=" + value + ", accions="/* + accions + "]"*/;
 	}
+
 	
 	public boolean theSame(Item item) {
 		boolean same = false;
@@ -73,49 +63,27 @@ public abstract class Item {
 			same = true;
 		}
 		
-		if(same==true && this.accions.size()==item.accions.size()) {
-			for(int i=0; i<accions.size(); i++) {
-				if(this.accions.get(i)!=item.accions.get(i)) {
-					same = false;
-				}
-			}
-		}else {
-			same = false;
-		}
+//		if(same==true && this.accions.size()==item.accions.size()) {
+//			for(int i=0; i<accions.size(); i++) {
+//				if(this.accions.get(i)!=item.accions.get(i)) {
+//					same = false;
+//				}
+//			}
+//		}else {
+//			same = false;
+//		}
 		
 		return same;
 	}
 	
 	public boolean addHability(Hability hability) {
-		boolean add = true;
-		
-		for(int i=0; i<this.accions.size() ; i++) {
-			if(hability.getName()==this.accions.get(i).getName()) {
-				add = false; 
-			}
-		}
-		
-		if(add==true) {
-			this.accions.add(hability);
-		}
+		boolean add = false;
 		
 		return add;
 	}
 	
 	public boolean deleteHability(Hability hability) {
 		boolean delete = false;
-		int delete_index = -1;
-		
-		for(int i=0 ; i<this.accions.size() ; i++) {
-			if(this.accions.get(i).getName()==hability.getName()) {
-				delete = true;
-				delete_index = i;
-			}
-		}
-		
-		if(delete==true) {
-			this.accions.remove(delete_index);
-		}
 		
 		return delete;
 	}
