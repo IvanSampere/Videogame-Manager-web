ebac3844af1c8a760f473279560f29737aa87b9b
Ivan Sampere <ivansampere@hotmail.com>
2020-04-23 19:31:12 +0200

    Se han cambiado los imports del fichero imports.sql hacia uno nuevo llamado data.sql. Tambien se han implementado el controller del Gestor, en el que se ha anyadido el indice  y la pagina habilidad. Se han hecho dos nuevas carpetas llamadas "static" que incluye el css y las imagenes; y la otra carpeta se llama "templates", que es donde se guardan los html, por el momento el index y hability. Por el momento el indice se ve bien y el único enlace funcional (Hability) funciona, al entrar a la pagina de habilidades se pueden ver bien todos los imports hechos a la base de datos. Las próximas cosas a hacer son: que se pueda anyadir una nueva habilidad desde el formulario, y que se puedan eliminar habilidades.

--- src/main/java/com/example/demo/GestorVideojuegosApplication.java
+++ src/main/java/com/example/demo/GestorVideojuegosApplication.java
@@ -1,29 +1,19 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.Repositories.HabilityRepository;
-import com.example.demo.Repositories.ItemRepositoy;
-
 @SpringBootApplication
 public class GestorVideojuegosApplication implements CommandLineRunner{
 	
-	@Autowired
-	ItemRepositoy repo;
-	@Autowired
-	HabilityRepository repoH;
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(GestorVideojuegosApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
 		
 	}
 
--- src/test/java/com/example/demo/GestorVideojuegosApplicationTests.java
+++ src/test/java/com/example/demo/GestorVideojuegosApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,41 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GestorVideojuegosApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void testValidateDni() {
-		boolean dniOk = false;
-		char letras[] = {'T','R','W','A','G','M','Y','F','P','D','X','B'
-				,'N','J','Z','S','Q','V','H','L','C','K','E','T'};
+	void testMenu() {
 		
-		
-		String dni1 = "53248P94M";
-		String dni = dni1;
-		
-		
-		
-		if(dni.length()==9) {
-			try {
-				
-				dni.toCharArray();
-				String subDni = dni.substring(0, 8);
-				int numDni = Integer.parseInt(subDni);
-				int result = numDni%23;
-				
-				if(dni.charAt(8)==letras[result]) {
-					dniOk = true;
-				}
-				
-			} catch(NumberFormatException e) {
-				System.out.println("Error en DNI");
-				dniOk = false;
-			}
-		}else {
-			dniOk = false;
-		}
-		
-		System.out.println(dniOk);
 	}
 }
